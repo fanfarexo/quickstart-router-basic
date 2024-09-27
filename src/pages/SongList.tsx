3cefc0826c28216822e54980d6ddfbc90408b1c3
fanfarexo <fanfarexo@gmail.com>
2024-09-27 11:26:04 +0900

    feat: 리액트 라우터 훅, useMatch

--- src/pages/SongList.tsx
+++ src/pages/SongList.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useMatch } from 'react-router-dom';
 import { SongType } from '../App';
 
 type Props = {
@@ -6,9 +6,15 @@ type Props = {
 };
 
 const SongList = (props: Props) => {
+  const pathMatch = useMatch('/songs/:id');
+  const param_id: number = pathMatch?.params.id ? parseInt(pathMatch.params.id, 10) : -1;
+
   const list = props.songs.map((song) => {
+    let cn = 'list-group-item';
+    cn += param_id === song.id ? ' list-group-item-secondary' : '';
+
     return (
-      <li className='list-group-item' key={song.id}>
+      <li className={cn} key={song.id}>
         <Link to={`/songs/${song.id}`} style={{ textDecoration: 'none' }}>
           {song.title} ( {song.musician} )
         </Link>
@@ -17,7 +23,7 @@ const SongList = (props: Props) => {
   });
 
   return (
-    <div className='card card-body'>
+    <div>
       <h2 className='mt-4 mb-2'>SongList</h2>
       <ul className='list-group'>{list}</ul>
       <Outlet />
